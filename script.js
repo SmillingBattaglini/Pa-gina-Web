d2c25bdf165bd519c2764d0f1b6df98d8603f863
Smilling Battaglini <battaglinismilling@gmail.com>
2022-10-02 21:17:58 -0300

    Repos. con VS CODE

---
+++ script.js
@@ -0,0 +1,9 @@
+function displayAlert(elemento_car){
+
+    alert("Your car is empty");
+}
+
+function dismissCookie() {
+    var cookie_element = document.querySelector('.footer');
+    cookie_element.remove();
+}
